dfcda8e14f0806dc993ebf330c403f41adf6121b
KYokoya05 <52030175+KYokoya05@users.noreply.github.com>
2019-07-19 00:16:15 +0900

    Add files via upload

--- top.php
+++ top.php
@@ -15,13 +15,13 @@
             </h1>
         </header>
         <main>
-            <form action="top.php" method="get">
+            <form action="top.php" method="post">
                 <input type="text" name="comment"><br>
                 <input type="submit"  value="送信">
             </form>
             <?php
 	            header('Access-Control-Allow-Origin: *');
-	            class Hotpepper{
+	            class Hotpepper {
                     /*URL*/
                     const URL = 'https://webservice.recruit.co.jp/hotpepper/gourmet/';
                     /*APIキー*/
@@ -30,38 +30,46 @@
                     private $apiVersion = 'v1';
 
                     /*URLへの接続*/
-                    function send($params)
+                    public function send($params)
                     {
-                    $url = sprintf('%s/%s/?key=%s&range=3&keyword=%s&count=100&format=json',
-                        self::URL,
-                        $this->apiVersion,
-                        $this->apiKey,
-                        $params
-                    );
+                        $url = sprintf("%s%s/?key=%s&range=3&keyword=%s&count=100&format=json",
+                            self::URL,
+                            $this->apiVersion,
+                            $this->apiKey,
+                            $params
+                        );
+
+                        $sh = file_get_contents($url);
+                        echo $sh;
                     
-                    if(!extension_loaded('curl'))
-                    {
-                        exit('cURL etension not loaded.');
-                    }
-                    $ch = curl_init();
-                    curl_setopt($ch, CURLOPT_URL, $url);
-                    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-                    curl_setopt($ch, CURLOPT_VERBOSE, 0);
-                    curl_setopt($ch, CURLOPT_HEADER, 0);
-                    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
-                    $response = curl_exec($ch);
-                    if($response === false)
-                    {
-                        exit('errorno:' . curl_getinfo($ch, CURLINFO_HTTP_CODE));
-                    }
+                        // if(!extension_loaded('curl'))
+                        // {
+                        //     exit('cURL etension not loaded.');
+                        // }
+                        // $ch = curl_init();
+                        // curl_setopt($ch, CURLOPT_URL, $url);
+                        // curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                        // curl_setopt($ch, CURLOPT_VERBOSE, false);
+                        // curl_setopt($ch, CURLOPT_HEADER, false);
+                        // curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+                        // echo $ch;
+                        // $response = curl_exec($ch);
+                        // echo $response;
+                        // if($response === false)
+                        // {
+                        //     exit('errorno:' . curl_getinfo($ch, CURLINFO_HTTP_CODE));
+                        // }
                     
-                    return file_get_contents($response);
+                        return  $sh;  //json_decode($response,true);
                     }
                 }
 
-                $hp = new Hotpepper();
-                $comment = $_GET['comment'];
-                $hp->send($comment);
+                if(isset($_POST["comment"]))
+                {
+                    $hp = new Hotpepper();
+                    $comment = $_POST["comment"];
+                    $hp->send($comment);
+                }
             ?>
         </main>
     </body>
